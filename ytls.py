01e042fafa2533ff1cdb64eb515a1252e5ebbe0c
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-08-23 16:12:21 -0300

    descargas las canciones

--- ytls.py
+++ ytls.py
@@ -1,11 +1,18 @@
 from youtube_dl import YoutubeDL
-import datetime
 import re
+import csv
 
 options = {
+    'outtmpl': 'music/%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'simulate': True,
     'noplaylist': True,
     'quiet': True,
+    'writethumbnail': True,
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }, {'key': 'EmbedThumbnail'},],
 }
 url = 'https://www.youtube.com/watch?v=fs2zUYQN9QA'
 pattern = re.compile(r'\d\d:\d\d:\d\d (.+)')
@@ -14,11 +21,22 @@ with YoutubeDL(options) as ydl:
     video = ydl.extract_info(url)
     value = video.get('description', None)
     names = pattern.findall(value)
+    videos = []
     for name in names:
-        video = ydl.extract_info(f'ytsearch1:{name}')['entries'][0]
-        print('name', name)
-        print('title', video.get('title'))
-        print('artist', video.get('artist'))
-        print('duration', datetime.timedelta(seconds=int(video.get('duration'))))
-        print('url', f'https://youtube.com/watch?v={video.get("id")}')
-        print()
+        search = f'ytsearch1:{name}'
+        video = ydl.extract_info(search)['entries'][0]
+        print(video.get('id'), video.get('artist'), video.get('title'))
+        videos.append({
+            'id': video.get('id'),
+            'artist': video.get('artist'),
+            'title': video.get('title')
+        })
+        break
+    print('download...')
+    ydl.params['simulate'] = False
+    ydl.params['quiet'] = False
+    for video in videos:
+        print(video.get('id'), video.get('artist'), video.get('title'))
+        url = f'https://youtube.com/watch?v={video.get("id")}'
+        ydl.download([url])
+
