3d99a2f23c6871d1c6d37a77f6f9b0faf2aaf1e7
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-08-23 14:46:26 -0300

    muestra información de los videos

--- ytls.py
+++ ytls.py
@@ -1,10 +1,24 @@
 from youtube_dl import YoutubeDL
+import datetime
+import re
 
 options = {
     'simulate': True,
+    'noplaylist': True,
+    'quiet': True,
 }
 url = 'https://www.youtube.com/watch?v=fs2zUYQN9QA'
+pattern = re.compile(r'\d\d:\d\d:\d\d (.+)')
+
 with YoutubeDL(options) as ydl:
     video = ydl.extract_info(url)
     value = video.get('description', None)
-    print(value)
+    names = pattern.findall(value)
+    for name in names:
+        video = ydl.extract_info(f'ytsearch1:{name}')['entries'][0]
+        print('name', name)
+        print('title', video.get('title'))
+        print('artist', video.get('artist'))
+        print('duration', datetime.timedelta(seconds=int(video.get('duration'))))
+        print('url', f'https://youtube.com/watch?v={video.get("id")}')
+        print()
