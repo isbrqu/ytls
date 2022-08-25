66a8f3a0c61af578af762edac688187a90964a8e
Isaac Bruno Quiroga <isbrqu@gmail.com>
2022-08-25 00:03:07 -0300

    guarda las canciones descargadas en una carpeta

--- ytls.py
+++ ytls.py
@@ -2,14 +2,14 @@ from youtube_dl import YoutubeDL
 import re
 import clipboard
 import json
-import os.path
+import os
 
 options = {
-    'outtmpl': 'music/%(title)s-%(id)s.%(ext)s',
     'simulate': True,
     'noplaylist': True,
     'quiet': True,
     'writethumbnail': True,
+    'format': 'mp4',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
@@ -17,8 +17,8 @@ options = {
     }, {'key': 'EmbedThumbnail'},],
 }
 pattern = re.compile(r'(?:\d\d:)?\d\d:\d\d (.+)')
-url_base = 'youtube.com/watch?v='
 url = ''
+url_base = 'https://youtube.com/watch?v='
 while url_base not in url:
     url = clipboard.paste()
     print(url)
@@ -28,22 +28,22 @@ while url_base not in url:
 with YoutubeDL(options) as ydl:
     source = ydl.extract_info(url)
     source_id = source.get("id")
-    file_name = f'videos-{source_id}.json'
+    folder_name = f'music-{source_id}'
+    file_name = f'{folder_name}/videos-{source_id}.json'
+    ydl.params['outtmpl'] = f'{folder_name}/%(title)s-%(id)s.%(ext)s'
+    os.makedirs(folder_name, exist_ok=True)
     if not os.path.exists(file_name):
         try:
             source_description = source.get('description', None)
             names = pattern.findall(source_description)
             videos = []
-            i = 0
             for name in names:
                 search = f'ytsearch1:{name}'
                 vid = ydl.extract_info(search)['entries'][0]
                 vid = {key: vid.get(key) for key in ['id', 'artist', 'title']}
                 vid['downloaded'] = False
+                print(vid)
                 videos.append(vid)
-                i += 1
-                if i == 3:
-                    break
         finally:
             with open(file_name, 'w') as file:
                 json.dump(videos, file, indent=2)
